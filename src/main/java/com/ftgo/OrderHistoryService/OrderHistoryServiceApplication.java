fde6576930c313c30bb31141c8c21a2a1d5ebc3b
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-10 20:08:19 +0900

    Initial commit

---
+++ src/main/java/com/ftgo/OrderHistoryService/OrderHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ftgo.OrderHistoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderHistoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftgo/OrderHistoryService/OrderHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftgo.OrderHistoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderHistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
