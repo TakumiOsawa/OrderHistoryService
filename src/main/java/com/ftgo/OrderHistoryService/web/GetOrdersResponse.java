b020ea88ca2c15db7519e4ac495eb812c33a7ce4
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-13 05:12:44 +0900

    Trying API Composition.

--- src/main/java/com/ftgo/OrderHistoryService/web/GetOrdersResponse.java
+++ src/main/java/com/ftgo/OrderHistoryService/web/GetOrdersResponse.java
@@ -3,13 +3,13 @@ package com.ftgo.OrderHistoryService.web;
 import com.ftgo.OrderHistoryService.domain.view.entity.ViewItem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class GetOrdersResponse {
     private List<ViewItem> items;
+
+    public GetOrdersResponse () {}
 }
