8f88869945542dc7779f7c982fba2ed98ffe56f4
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-13 06:37:06 +0900

    Trying CQRS.

--- src/main/java/com/ftgo/OrderHistoryService/domain/order/OrderLineItems.java
+++ src/main/java/com/ftgo/OrderHistoryService/domain/order/OrderLineItems.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderLineItems {
-    private final List<OrderLineItem> value = new ArrayList<>();
+    private List<OrderLineItem> value;
 }
