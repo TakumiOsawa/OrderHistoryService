c20ff25499cb630a43b355880236c5abc61b4dce
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-13 05:18:49 +0900

    Trying API Composition.

--- src/main/java/com/ftgo/OrderHistoryService/web/OrderHistoryController.java
+++ src/main/java/com/ftgo/OrderHistoryService/web/OrderHistoryController.java
@@ -1,11 +1,14 @@
 package com.ftgo.OrderHistoryService.web;
 
 import com.ftgo.OrderHistoryService.OrderHistoryService;
+import com.ftgo.OrderHistoryService.domain.view.entity.ViewItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OrderHistoryController {
@@ -24,6 +27,8 @@ public class OrderHistoryController {
 
     @GetMapping("/view/{consumerId}")
     public GetOrdersResponse getViewItems(@PathVariable long consumerId) {
-        return new GetOrdersResponse(orderHistoryService.getViewByComposition(consumerId));
+        List<ViewItem> result = orderHistoryService.getViewByComposition(consumerId);
+        logger.info("Get Information.");
+        return new GetOrdersResponse(result);
     }
 }
